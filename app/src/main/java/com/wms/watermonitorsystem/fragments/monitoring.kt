5158bfe5befc87cbe98fb39e21a84312ccd3e5fe
afranio-viana <afranio.a164@gmail.com>
2022-08-31 19:39:25 -0400

    Nova plotagem de temperatura, umidade e data!

--- app/src/main/java/com/wms/watermonitorsystem/fragments/monitoring.kt
+++ app/src/main/java/com/wms/watermonitorsystem/fragments/monitoring.kt
@@ -6,14 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.firebase.database.*
 import com.wms.watermonitorsystem.R
+import kotlin.reflect.typeOf
 
 class monitoring : Fragment() {
     private lateinit var database: DatabaseReference
+    private lateinit var lastQuery: Query
     companion object {
-        private const val TAG = "KotlinQueryActivity"
+        const val TAG = "KotlinQueryActivity"
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,21 +25,26 @@ class monitoring : Fragment() {
         // Inflate the layout for this fragment
         var mac= arguments?.getString("key")
         database= FirebaseDatabase.getInstance().getReference("User2")
-        database.child(mac.toString()).addValueEventListener(object: ValueEventListener{
+        lastQuery = database.child(mac.toString()).orderByKey().limitToLast(1)
+        lastQuery.addValueEventListener(object: ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
-               if(snapshot.exists()){
-                   val temperature=view?.findViewById<TextView>(R.id.temperature)
-                   temperature?.text=snapshot.child("temperature").getValue().toString()+" °C"
-                   val humidity=view?.findViewById<TextView>(R.id.humidity)
-                   humidity?.text=snapshot.child("humidity").getValue().toString()+" %"
-               }
+                if(snapshot.exists()){
+                    val temperature=view?.findViewById<TextView>(R.id.temperature)
+                    val humidity=view?.findViewById<TextView>(R.id.humidity)
+                    for( child:DataSnapshot in snapshot.children){
+                        val temp=(child.child("temperature").getValue().toString()).toFloat().toInt()
+                        val humi=(child.child("humidity").getValue().toString()).toFloat().toInt()
+                        temperature?.text=temp.toString()+" °C"
+                        humidity?.text=humi.toString()+" %"
+
+                    }
+                }
             }
             override fun onCancelled(databaseError: DatabaseError) {
-                // Getting Post failed, log a message
-                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
-                // ...
+                Log.w(TAG, "loadPost:onCancelledd", databaseError.toException())
             }
-        } )
+        })
+
         //Toast.makeText(this.context, database.toString(), Toast.LENGTH_SHORT).show()
         return inflater.inflate(R.layout.fragment_monitoring, container, false)
     }
--- app/src/main/java/com/wms/watermonitorsystem/fragments/reports.kt
+++ app/src/main/java/com/wms/watermonitorsystem/fragments/reports.kt
@@ -1,20 +1,75 @@
 package com.wms.watermonitorsystem.fragments
 
+import android.icu.text.SimpleDateFormat
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.google.firebase.database.*
 import com.wms.watermonitorsystem.R
+import java.security.KeyStore
+import java.util.*
+import kotlin.collections.ArrayList
 
 class reports : Fragment() {
-
+    private lateinit var database: DatabaseReference
+    private lateinit var lastQuery: Query
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        var mac= arguments?.getString("key")
+        database= FirebaseDatabase.getInstance().getReference("User2").child(mac.toString())
+        //Toast.makeText(this.context, database.toString(), Toast.LENGTH_SHORT).show()
+        /*database.addValueEventListener(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val dataVal= ArrayList<KeyStore.Entry>()
+                if(snapshot.hasChildren()){
+                    for(myDataSnapshot :DataSnapshot in snapshot.children){
+                        //val dataPoint:DataPoint=myDataSnapshot.getValue(DataPoint:Object)
+                        //Toast.makeText(context, myDataSnapshot.toString(), Toast.LENGTH_SHORT).show()
+                        val time=myDataSnapshot.child("epoch_time").getValue().toString()
+                        val data_hora=timestampConvert(time)
+                        val temp=(myDataSnapshot.child("temperature").getValue().toString()).toFloat().toInt()
+                        val xvalue=ArrayList<String>()
+                        xvalue.add(data_hora)
+                    }
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w(monitoring.TAG, "loadPost:onCancelledd", databaseError.toException())
+            }
+        })*/
+        lastQuery = database.orderByKey().limitToLast(1)
+        lastQuery.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    val data=view?.findViewById<TextView>(R.id.textView6)
+                    for( child: DataSnapshot in snapshot.children){
+                        val time=child.child("epoch_time").getValue().toString()
+                        val data_hora=timestampConvert(time)
+
+                    data?.text=data_hora.toString()
+                    }
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w(monitoring.TAG, "loadPost:onCancelledd", databaseError.toException())
+            }
+        })
         return inflater.inflate(R.layout.fragment_reports, container, false)
+    }
+
+    fun timestampConvert(time:String): String {
+        val timeD:Date= Date(time.toLong()*1000)
+        val sdf : SimpleDateFormat= SimpleDateFormat("EEEE dd-MM-yyy HH:mm")
+        val data_hora :String=sdf.format(timeD)
+        return data_hora
     }
 
 }
